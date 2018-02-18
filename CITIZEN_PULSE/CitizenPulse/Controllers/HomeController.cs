7826c6116e5ba669da615ac06f334f1abb5db34c
Aneeq <aneeq@eikeconsulting.com>
2018-02-19 00:43:47 +0500

    some renaming and generate template files

--- CITIZEN_PULSE/CitizenPulse/Controllers/HomeController.cs
+++ CITIZEN_PULSE/CitizenPulse/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace WebApplication1.Controllers
+namespace CitizenPulse.Controllers
 {
     public class HomeController : Controller
     {
--- CITIZEN_PULSE/CitizenPulse/Models/CitizenPulse.Designer.cs
+++ CITIZEN_PULSE/CitizenPulse/Models/CitizenPulse.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\ahashmi\Documents\Projects\FYP-Dua,Aleena,Nida\CITIZEN_PULSE\CitizenPulse\Models\CitizenPulse.edmx'. 
+﻿// T4 code generation is enabled for model 'c:\users\haris bhai\documents\visual studio 2015\Projects\WebApplication1\WebApplication1\Models\CitizenPulse.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
